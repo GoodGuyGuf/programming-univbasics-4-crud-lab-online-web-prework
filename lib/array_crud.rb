cc0a551d8c7689c77eb253575842c1b8759c09ca
Ethan <ethan_gustafson@outlook.com>
2019-10-26 18:06:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-   anime = ["One piece", "My hero academia", "Attack on Titan", "Bleach", "arrays!"]
+   anime = ["wow", "My hero academia", "Attack on Titan", "Bleach", "arrays!"]
   anime.shift
 end
 
