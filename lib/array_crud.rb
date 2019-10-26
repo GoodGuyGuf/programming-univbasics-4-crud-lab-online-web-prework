59fd7b74ec7211f12c59ca02ee3263e710b8e0e8
Ethan <ethan_gustafson@outlook.com>
2019-10-26 17:55:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  anime << "Hunter x Hunter"
+  anime.push("Hunter x Hunter")
 end
 
 def add_element_to_start_of_array(array, element)
