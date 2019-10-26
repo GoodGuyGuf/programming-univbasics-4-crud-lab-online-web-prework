03f15fba702ba8291fbc105d920466a7342ac4f2
Ethan <ethan_gustafson@outlook.com>
2019-10-26 17:58:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   anime = ["One piece", "My hero academia", "Attack on Titan", "Bleach"]
   element = "Hunter x Hunter"
+  anime/push()
 end
 
 def add_element_to_start_of_array(array, element)
